73bc97a16ecc9c508fbc441c4dfa86da1771181d
Kulwinder Kour <abhishekswami3330@gmail.com>
2025-07-12 20:55:23 +0530

    Server.ts some changes

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,8 @@ import app from './app';
 import logger from './utils/logger';
 
 const PORT = process.env.PORT || 3001;
-const HOST = '0.0.0.0'; // Important for Render
 
-app.listen(PORT, HOST, () => {
+app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
-  console.log(`🚀 Server running on http://${HOST}:${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
